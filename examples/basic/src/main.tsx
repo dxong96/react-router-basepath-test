784a18c2f9dd26da29318f724314695cd1564637
Dexing <ongdexing@gmail.com>
2024-01-05 16:09:31 +0800

    Update basic example to serve at basepath /cbniform

--- examples/basic/src/main.tsx
+++ examples/basic/src/main.tsx
@@ -7,7 +7,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/cbniform">
       <App />
     </BrowserRouter>
   </React.StrictMode>
