f5ff7cab2ce950b13795b8018b73604a5207b62b
Dexing <ongdexing@gmail.com>
2024-01-09 22:35:25 +0800

    Update sample to work with nginx docker image and update base path to /e727.

--- examples/basic/src/main.tsx
+++ examples/basic/src/main.tsx
@@ -7,7 +7,7 @@ import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/cbniform">
+    <BrowserRouter basename="/e727">
       <App />
     </BrowserRouter>
   </React.StrictMode>
